7a64d27d21ab035477f7ed562c304603814b1f4a
ian <tablate.ian@gmail.com>
2019-06-10 01:26:50 -0400

    car backend with initial front-end template

---
+++ server/src/main/java/com/ian/developer/demo/Car.java
@@ -0,0 +1,17 @@
+package com.ian.developer.demo;
+
+import lombok.*;
+
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Car {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private @NonNull String name;
+}
